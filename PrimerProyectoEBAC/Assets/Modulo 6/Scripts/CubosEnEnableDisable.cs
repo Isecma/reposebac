0abed09aa516e11f6e246746a3d0a6182897039b
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-03-21 18:45:50 -0600

    Corrección de Práctica Módulo 6
    -Se corrigieron los scripts para instanciar cubos en Awake(), OnEnable() y OnDisable()

--- PrimerProyectoEBAC/Assets/Modulo 6/Scripts/CubosEnEnableDisable.cs
+++ PrimerProyectoEBAC/Assets/Modulo 6/Scripts/CubosEnEnableDisable.cs
@@ -4,31 +4,8 @@ using UnityEngine;
 
 public class CubosEnEnableDisable : MonoBehaviour
 {
-    GameObject objToSpawn;
-    Vector3[] vertices = {
-                     new Vector3(0,0,0), //vertice 0
-                     new Vector3(1,0,0), //vertice 1
-                     new Vector3(1,1,0), //vertice 2
-                     new Vector3(0,1,0), //vertice 3
-                     new Vector3(0,1,1), //vertice 4
-                     new Vector3(1,1,1), //vertice 5
-                     new Vector3(1,0,1), //vertice 6
-                     new Vector3(0,0,1), //vertice 7
-            };
-    int[] triangulos = {
-            0,2,1, //cara 1
-            0,3,2,
-            2,3,4, //cara 2
-            2,4,5,
-            1,2,5, //cara 3
-            1,5,6,
-            0,7,4, //cara 4
-            0,4,3,
-            5,4,7, //cara 5
-            5,7,6,
-            0,6,7, //cara 6
-            0,1,6,
-     };
+    public GameObject Prefabcubo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -42,38 +19,12 @@ public class CubosEnEnableDisable : MonoBehaviour
     }
     private void OnEnable()
     {
-        objToSpawn = new GameObject("PrimerCubo");
-        objToSpawn.AddComponent<MeshFilter>();
-        var meshFilter = objToSpawn.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        objToSpawn.AddComponent<BoxCollider>();
-        var boxCollider = objToSpawn.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        objToSpawn.AddComponent<MeshRenderer>();
-        var meshRendererMaterial = objToSpawn.GetComponent<MeshRenderer>().material;
-        meshRendererMaterial.color = Color.white;
-        objToSpawn.transform.position = new Vector3(Random.value, Random.value, Random.value);
+        Instantiate<GameObject>(Prefabcubo);
+        Prefabcubo.transform.position = new Vector3(Random.Range(-6, 6), Random.Range(-6, 6), Random.Range(-6, 6));
     }
     private void OnDisable()
     {
-        objToSpawn = new GameObject("Cubo");
-        objToSpawn.AddComponent<MeshFilter>();
-        var meshFilter = objToSpawn.GetComponent<MeshFilter>().mesh;
-        meshFilter.Clear();
-        meshFilter.vertices = vertices;
-        meshFilter.triangles = triangulos;
-        meshFilter.Optimize();
-        meshFilter.RecalculateNormals();
-        objToSpawn.AddComponent<BoxCollider>();
-        var boxCollider = objToSpawn.GetComponent<BoxCollider>();
-        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
-        objToSpawn.AddComponent<MeshRenderer>();
-        var meshRendererMaterial = objToSpawn.GetComponent<MeshRenderer>().material;
-        meshRendererMaterial.color = Color.white;
-        objToSpawn.transform.position = new Vector3(Random.Range(-6,6), Random.Range(-6, 6), Random.Range(-6, 6));
+        Instantiate<GameObject>(Prefabcubo);
+        Prefabcubo.transform.position = new Vector3(Random.Range(-6,6), Random.Range(-6, 6), Random.Range(-6, 6));
     }
 }
